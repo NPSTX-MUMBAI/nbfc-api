89c64cfc63a0b49d2378d567a82c5b86dbee9067
bhandekuanl16 <bhandekunal16@gmail.com>
2023-09-12 15:33:16 +0530

    add two module's auth and plan

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PlanModule } from './plan/plan.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, PlanModule],
   controllers: [AppController],
   providers: [AppService],
 })
