6c87378c48c86f6006e4eaaf479cfd0d1e8f9c9b
bhandekuanl16 <bhandekunal16@gmail.com>
2023-09-12 18:30:14 +0530

    lld';

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 require('dotenv').config();
 
 async function bootstrap() {
   Logger.log('localHost :' + process.env.LOCALHOST, 'main.ts');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.LOCALHOST);
 }
 bootstrap();
