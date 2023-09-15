256eaff1a344fea76e8f530ac0e42b1dcf721edb
SudesSur <sudesh.surve@npstx.com>
2023-09-15 18:00:40 +0530

    plans serviuce

--- src/plan/plan.service.ts
+++ src/plan/plan.service.ts
@@ -109,17 +109,21 @@ export class PlanService {
       for (const item of data) {
           const accountId = generateRandomString(10);
           const accountNo = generateRandomNumber();
+          let loanStartDate:any=new Date(item.loanStartDate).getTime();
+          let loanEndDate:any=new Date(item.loanEndDate).getTime();
+          let emidueDate:any=new Date(item.emiDueDate).getTime();
+          let emiDate:any=new Date(item.emiDate).getTime();
           const query2 = await this.plan.createPlan({
             accountId: accountId,
             accountNo: accountNo,
-            emiDate: item.emiDate,
+            emiDate: emiDate,
             disbursementAmount: item.disbursementAmount,
             emiAmount: item.emiAmount,
-            loanStartDate: item.loanStartDate,
-            loanEndDate: item.loanEndDate,
-            emiDueDate: item.emiDueDate,
+            loanStartDate:loanStartDate,
+            loanEndDate: loanEndDate,
+            emiDueDate:emidueDate ,
             address: item.address,
-            loanType: item.loanType,
+            loanType: item.loanType,  
             firstName: item.firstName,
             lastName: item.lastName,
             mobileNo: item.mobileNo,
